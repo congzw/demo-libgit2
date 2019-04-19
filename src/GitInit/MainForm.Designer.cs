f075207838b1b4283f838e53ad323e58a249b955
congzw <2981810300@qq.com>
2019-04-19 10:59:47 +0800

    init

---
+++ src/GitInit/MainForm.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace GitInit
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtRepo = new System.Windows.Forms.TextBox();
+            this.btnInit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtMessage.Location = new System.Drawing.Point(12, 12);
+            this.txtMessage.Multiline = true;
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(560, 139);
+            this.txtMessage.TabIndex = 14;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 193);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 12);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Repository:";
+            // 
+            // txtRepo
+            // 
+            this.txtRepo.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtRepo.Location = new System.Drawing.Point(100, 190);
+            this.txtRepo.Name = "txtRepo";
+            this.txtRepo.Size = new System.Drawing.Size(389, 21);
+            this.txtRepo.TabIndex = 12;
+            // 
+            // btnInit
+            // 
+            this.btnInit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnInit.Location = new System.Drawing.Point(497, 188);
+            this.btnInit.Name = "btnInit";
+            this.btnInit.Size = new System.Drawing.Size(75, 23);
+            this.btnInit.TabIndex = 11;
+            this.btnInit.Text = "Init";
+            this.btnInit.UseVisualStyleBackColor = true;
+            this.btnInit.Click += new System.EventHandler(this.btnInit_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 223);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtRepo);
+            this.Controls.Add(this.btnInit);
+            this.Name = "MainForm";
+            this.Text = "GitInit";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtRepo;
+        private System.Windows.Forms.Button btnInit;
+    }
+}
+
---
+++ src/GitInit/MainForm.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows.Forms;
+using GitInit.ViewModel;
+
+namespace GitInit
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            Main = new MainFormVo();
+        }
+
+        public MainFormVo Main { get; set; }
+
+        private void btnInit_Click(object sender, EventArgs e)
+        {
+            var repo = this.txtRepo.Text.TrimEnd();
+            if (string.IsNullOrWhiteSpace(repo))
+            {
+                MessageBox.Show("仓库名不合法" + repo);
+                return;
+            }
+
+            Main.Delete(repo);
+            Main.DirectoryCopy(@".\_Template", repo, true);
+            var slnPath = String.Format(@".\{0}\src\foo.sln", repo);
+            var newSlnPath = String.Format(@".\{0}\src\{0}.sln", repo);
+            Main.ChangeRepositoryAsSln(slnPath, newSlnPath);
+            Main.Init(repo);
+        }
+        
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            this.txtRepo.Text = "MyRepository";
+        }
+    }
+}
